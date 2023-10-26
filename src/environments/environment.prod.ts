dcdc867d4398fb24b5b6d75a72fa6308e8df3525
rumabe97 <rumabe97@gmail.com>
2023-10-26 16:38:58 +0200

    Vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://glucemyback.herokuapp.com/api/',
+  apiUrl: 'https://glucemy-back-mvjm46pj6-rubens-projects-260947e4.vercel.app/api/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'https://glucemyback.herokuapp.com/api/',
+    apiUrl: 'https://glucemy-back-mvjm46pj6-rubens-projects-260947e4.vercel.app/api/',
 };
 
 /*
