61584f4600bd3ec24c4d0e123a0d348ec33728ed
rumabe97 <rumabe97@gmail.com>
2023-10-26 18:01:24 +0200

    Vercel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'https://glucemy-back-mvjm46pj6-rubens-projects-260947e4.vercel.app/api/',
+    apiUrl: 'https://glucemy-back.vercel.app/',
 };
 
 /*
